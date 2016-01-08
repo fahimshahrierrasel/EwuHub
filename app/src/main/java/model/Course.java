cdb6f2bfde877f6e938ed20baf2d1a29fd39f3fb
Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2016-01-09 01:00:54 +0600

    Course and CourseL java class added.

---
+++ app/src/main/java/model/Course.java
@@ -0,0 +1,139 @@
+package model;
+
+public class Course
+{
+    protected String courseTitle;
+    protected String courseCode;
+    protected int section;
+    protected int timeFrom;
+    protected int timeTo;
+    protected String weekDay;
+
+
+    public Course()
+    {
+        courseTitle = "NULL";
+        courseCode = "NULL";
+        section = 0;
+        timeFrom = 0000;
+        timeTo = 0000;
+        weekDay = "NULL";
+    }
+
+    public Course(String courseTitle, String courseCode, int section, int timeFrom, int timeTo, String weekDay)
+    {
+        this.courseTitle = courseTitle;
+        this.courseCode = courseCode;
+        this.section = section;
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+        this.weekDay = weekDay;
+    }
+
+    public String getCourseTitle() {
+
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getTimeFrom() {
+        return timeFrom;
+    }
+
+    public void setTimeFrom(int timeFrom) {
+        this.timeFrom = timeFrom;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public int getTimeTo() {
+        return timeTo;
+    }
+
+    public void setTimeTo(int timeTo) {
+        this.timeTo = timeTo;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public boolean identiClassWithLab(String labDay)
+    {
+        boolean result = false;
+        for(int loop1 = 0; loop1 < weekDay.length(); loop1++)
+        {
+            if (weekDay.charAt(loop1) == labDay.charAt(0))
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+    public boolean identicalClass(Course otherCourse)
+    {
+        boolean result = false;
+        String tempWeekDay = otherCourse.getWeekDay();
+
+        for(int loop1 = 0; loop1 < tempWeekDay.length(); loop1++)
+        {
+            for(int loop2 = 0; loop2 < weekDay.length(); loop2++)
+            {
+                if(tempWeekDay.charAt(loop1) == weekDay.charAt(loop2))
+                {
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean findConfliction(Course otherCourse)
+    {
+        boolean result = false;
+        if(identicalClass(otherCourse))
+        {
+            if(otherCourse.timeFrom > timeTo || timeFrom > otherCourse.timeTo)
+            {
+                result = true;
+            }
+        }
+        else
+            result = true;
+
+        return result;
+    }
+
+    public String printCourse()
+    {
+        return (courseTitle + courseCode + "     " + section + "     " +
+                timeFrom + "     " + timeTo + "     " + weekDay);
+    }
+
+    @Override
+    public String toString() {
+        return courseTitle+courseCode;
+    }
+}
---
+++ app/src/main/java/model/CourseL.java
@@ -0,0 +1,173 @@
+package model;
+
+public class CourseL extends Course
+{
+    private int labTimeFrom;
+    private int labTimeTo;
+    private String labWeekDay;
+
+    public CourseL()
+    {
+        super();
+        labTimeFrom = 0000;
+        labTimeTo = 0000;
+        labWeekDay = "NULL";
+
+    }
+    public CourseL(String courseTitle, String courseCode, int section, int timeFrom, int timeTo, String weekDay, int labTimeFrom, int labTimeTo, String labWeekDay)
+    {
+        super(courseTitle, courseCode, section, timeFrom, timeTo, weekDay);
+        this.labWeekDay = labWeekDay;
+        this.labTimeFrom = labTimeFrom;
+        this.labTimeTo = labTimeTo;
+
+    }
+
+
+    public int getLabTimeFrom() {
+        return labTimeFrom;
+    }
+
+    public void setLabTimeFrom(int labTimeFrom) {
+        this.labTimeFrom = labTimeFrom;
+    }
+
+    public int getLabTimeTo() {
+        return labTimeTo;
+    }
+
+    public void setLabTimeTo(int labTimeTo) {
+        this.labTimeTo = labTimeTo;
+    }
+
+    public String getLabWeekDay() {
+        return labWeekDay;
+    }
+
+    public void setLabWeekDay(String labWeekDay) {
+        this.labWeekDay = labWeekDay;
+    }
+
+
+    public boolean identicalClass(CourseL otherCourse)
+    {
+        boolean result = false;
+        for(int loop1 = 0; loop1 < otherCourse.weekDay.length(); loop1++)
+        {
+            for(int loop2 = 0; loop2 < weekDay.length(); loop2++)
+            {
+                if (weekDay.charAt(loop2) == otherCourse.weekDay.charAt(loop1))
+                {
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+
+    public boolean findConfliction(CourseL otherCourse)
+    {
+        boolean result = false;
+        if((identicalClass(otherCourse) && identiClassWithLab(otherCourse.labWeekDay)) && (labWeekDay.equals(otherCourse.labWeekDay)))
+        {
+            if((otherCourse.timeFrom > timeTo || timeFrom > otherCourse.timeTo) && (otherCourse.timeFrom > labTimeTo || labTimeFrom > otherCourse.timeTo) && (otherCourse.labTimeFrom > labTimeTo || labTimeFrom > otherCourse.labTimeTo))
+            {
+                result = true;
+            }
+
+        }
+        else if((identicalClass(otherCourse)  && identiClassWithLab(otherCourse.labWeekDay) && !labWeekDay.equals(otherCourse.labWeekDay)))
+        {
+            if((otherCourse.timeFrom > timeTo || timeFrom > otherCourse.timeTo) && (otherCourse.timeFrom > labTimeTo || labTimeFrom > otherCourse.timeTo))
+            {
+                result = true;
+            }
+        }
+        else if(identicalClass(otherCourse) && !identiClassWithLab(otherCourse.labWeekDay) && labWeekDay.equals(otherCourse.labWeekDay))
+        {
+            if((otherCourse.timeFrom > timeTo || timeFrom > otherCourse.timeTo) && (otherCourse.labTimeFrom >labTimeTo || labTimeFrom > otherCourse.labTimeTo))
+            {
+                result = true;
+            }
+        }
+        else if(!identicalClass(otherCourse) && identiClassWithLab(otherCourse.labWeekDay) && labWeekDay.equals(otherCourse.labWeekDay))
+        {
+            if((otherCourse.timeFrom > labTimeTo || labTimeFrom > otherCourse.timeTo) && (otherCourse.labTimeFrom >labTimeTo || labTimeFrom > otherCourse.labTimeTo))
+            {
+                result = true;
+            }
+        }
+        else if(identicalClass(otherCourse) && !identiClassWithLab(otherCourse.labWeekDay) && !labWeekDay.equals(otherCourse.labWeekDay))
+        {
+            if(otherCourse.timeFrom > timeTo || timeFrom > otherCourse.timeTo)
+            {
+                result = true;
+            }
+        }
+        else if (!identicalClass(otherCourse) && identiClassWithLab(otherCourse.labWeekDay) && !labWeekDay.equals(otherCourse.labWeekDay))
+        {
+            if(otherCourse.timeFrom > labTimeTo || labTimeFrom > otherCourse.timeTo)
+            {
+                result = true;
+            }
+        }
+        else if (!identicalClass(otherCourse) && !identiClassWithLab(otherCourse.labWeekDay) && labWeekDay.equals(otherCourse.labWeekDay))
+        {
+            if(otherCourse.labTimeFrom > labTimeTo || labTimeFrom > otherCourse.labTimeTo)
+            {
+                result = true;
+            }
+        }
+        else
+            result = true;
+
+        return  result;
+
+    }
+
+    public boolean findConfliction(Course otherCourse)
+    {
+        boolean result = false;
+
+        String otherWeekDay = otherCourse.getWeekDay();
+
+        if(identicalClass(otherCourse) && otherCourse.identiClassWithLab(labWeekDay))
+        {
+            if((otherCourse.getTimeFrom() > timeTo || timeFrom > otherCourse.getTimeTo()) && (otherCourse.getTimeFrom() > labTimeTo || labTimeFrom > otherCourse.getTimeTo()))
+            {
+                result = true;
+            }
+        }
+        else if(identicalClass(otherCourse) && !otherCourse.identiClassWithLab(labWeekDay))
+        {
+            if(otherCourse.getTimeFrom() > timeTo || timeFrom > otherCourse.getTimeTo())
+            {
+                result = true;
+            }
+        }
+        else if(otherCourse.identiClassWithLab(labWeekDay) && !identicalClass(otherCourse))
+        {
+            if(otherCourse.getTimeFrom() > labTimeTo || labTimeFrom > otherCourse.getTimeTo())
+            {
+                result = true;
+            }
+        }
+        else
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public String printCourse()
+    {
+        return (courseTitle + courseCode + "     " + section + "     "  +
+                timeFrom + "     " + timeTo + "     " + weekDay + "     " +
+                labTimeFrom + "     " + labTimeTo + "     " + labWeekDay);
+    }
+
+}
