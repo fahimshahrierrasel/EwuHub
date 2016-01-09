ba951ca7d342879fdc80be239ddc8779f32299dc
Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2016-01-10 00:11:53 +0600

    Database linker and Data collector Class added & finished.
    DatabaseHelper, CourseDataSource, CourseLDataSource, LabDataSource class
    added and its done.

---
+++ app/src/main/java/com/treebricks/ewuhub/database/LabDataSource.java
@@ -0,0 +1,50 @@
+package com.treebricks.ewuhub.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class LabDataSource
+{
+    public static final String LOGTAG = "ProjectZero";
+    Cursor cursor = null;
+    private String table = "LABCOURSES";
+    private static final String[] allCloumns = {
+            "LAB"
+    };
+    public ArrayList<String> findAll(Context context)
+    {
+        DatabaseHelper dbhelper = new DatabaseHelper(context, table);
+        ArrayList<String> allLabs = new ArrayList<String>();
+        try {
+            dbhelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            dbhelper.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+        cursor = dbhelper.query(table, allCloumns, null, null, null, null, null);
+        Log.i(LOGTAG, "Database Successfully Connected!");
+
+        if (cursor.moveToFirst()) {
+            do {
+                String string = cursor.getString(cursor.getColumnIndex("LAB"));
+                allLabs.add(string);
+            } while (cursor.moveToNext());
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+        return allLabs;
+    }
+}
