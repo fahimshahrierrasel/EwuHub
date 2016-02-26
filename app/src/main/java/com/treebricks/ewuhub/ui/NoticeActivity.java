3fb7f2d6cc9823bf9ffb535066c134cd18ede355
Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2016-02-27 01:08:27 +0600

    Notice Board features complete.

--- app/src/main/java/com/treebricks/ewuhub/ui/NoticeActivity.java
+++ app/src/main/java/com/treebricks/ewuhub/ui/NoticeActivity.java
@@ -1,16 +1,27 @@
 package com.treebricks.ewuhub.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 import com.treebricks.ewuhub.R;
+import com.treebricks.ewuhub.view.NoticeAdapter;
+import com.treebricks.ewuhub.view.NoticeView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
-public class NoticeActivity extends AppCompatActivity {
-
+public class NoticeActivity extends AppCompatActivity
+{
+    String jsonString;
+    JSONObject jsonObject;
+    JSONArray jsonArray;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<NoticeView> recycleView = new ArrayList<NoticeView>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +29,55 @@ public class NoticeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        jsonString = getIntent().getExtras().getString("JSON_DATA");
+
+
+
+        createRecyclerView();
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.notice_recycler_view);
+
+
+
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new NoticeAdapter(recycleView, NoticeActivity.this, jsonString);
+        mRecyclerView.setAdapter(mAdapter);
+
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void createRecyclerView()
+    {
+        try {
+            jsonObject = new JSONObject(jsonString);
+            jsonArray = jsonObject.getJSONArray("Notice");
+            int count = 0;
+            String noticeTitle, noticeDate, noticeUrl;
+            while(count < jsonArray.length())
+            {
+                JSONObject jObject = jsonArray.getJSONObject(count);
+                noticeTitle = jObject.getString("NoticeTitle");
+                noticeDate = jObject.getString("NoticeDate");
+                noticeUrl = jObject.getString("NoticeUrl");
+                NoticeView viewer  = new NoticeView(noticeTitle,noticeDate,noticeUrl);
+                recycleView.add(viewer);
+
+                count++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ app/src/main/java/com/treebricks/ewuhub/view/NoticeView.java
@@ -0,0 +1,41 @@
+package com.treebricks.ewuhub.view;
+
+/**
+ * Created by fahimshahrier on 2/26/16.
+ */
+public class NoticeView
+{
+    private String noticeTitle;
+    private String noticeDate;
+    private String noticeUrl;
+
+    public NoticeView(String noticeTitle, String noticeDate, String noticeUrl) {
+        this.noticeTitle = noticeTitle;
+        this.noticeDate = noticeDate;
+        this.noticeUrl = noticeUrl;
+    }
+
+    public String getNoticeTitle() {
+        return noticeTitle;
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        this.noticeTitle = noticeTitle;
+    }
+
+    public String getNoticeDate() {
+        return noticeDate;
+    }
+
+    public void setNoticeDate(String noticeDate) {
+        this.noticeDate = noticeDate;
+    }
+
+    public String getNoticeUrl() {
+        return noticeUrl;
+    }
+
+    public void setNoticeUrl(String noticeUrl) {
+        this.noticeUrl = noticeUrl;
+    }
+}
